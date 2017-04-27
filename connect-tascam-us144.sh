60ed10954a18423334218bd0796f8634d04de3dd
Unknown <kn.vonkiedrowski@gmail.com>
2017-04-28 01:23:20 +0200

    update

--- connect-tascam-us144.sh
+++ connect-tascam-us144.sh
@@ -2,7 +2,7 @@
 echo -n 0000:00:12.2 | sudo tee -a /sys/bus/pci/drivers/ehci-pci/unbind
 echo -n 0000:00:13.2 | sudo tee -a /sys/bus/pci/drivers/ehci-pci/unbind
 cat /proc/asound/cards
-read -n 1 -s -p "Interface anschließen"
+read -n 1 -s -p "Please connect interface"
 cat /proc/asound/cards
 nohup cadence
 exit
